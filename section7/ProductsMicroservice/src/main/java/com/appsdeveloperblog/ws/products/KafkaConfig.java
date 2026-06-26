e6e9cee8ccf5cdd2aa4f51634d2434d1c24b87cc
Shen Tang <adatang23@yahoo.com>
2026-06-25 21:17:38 -0400

    Fixed the syntax of environment variables

--- section7/ProductsMicroservice/src/main/java/com/appsdeveloperblog/ws/products/KafkaConfig.java
+++ section7/ProductsMicroservice/src/main/java/com/appsdeveloperblog/ws/products/KafkaConfig.java
@@ -19,25 +19,25 @@ import com.appsdeveloperblog.ws.products.service.ProductCreatedEvent;
 @Configuration
 public class KafkaConfig {
 	
-	@Value("{spring.kafka.producer.bootstrap-servers}")
+	@Value("${spring.kafka.producer.bootstrap-servers}")
 	private String bootstrapServers;
 	
-	@Value("{spring.kafka.producer.key-serializer}")
+	@Value("${spring.kafka.producer.key-serializer}")
 	private String keySerializer;
 	
-	@Value("{spring.kafka.producer.value-serializer}")
+	@Value("${spring.kafka.producer.value-serializer}")
 	private String valueSerializer;
 	
-	@Value("{spring.kafka.producer.acks}")
+	@Value("${spring.kafka.producer.acks}")
 	private String acks;
 
-	@Value("{spring.kafka.producer.properties.delivery.timeout.ms}")
+	@Value("${spring.kafka.producer.properties.delivery.timeout.ms}")
 	private String deliveryTimeout;
 	
-	@Value("{spring.kafka.producer.properties.linger.ms}")
+	@Value("${spring.kafka.producer.properties.linger.ms}")
 	private String linger;
 	
-	@Value("{spring.kafka.producer.properties.request.timeout.ms}")
+	@Value("${spring.kafka.producer.properties.request.timeout.ms}")
 	private String requestTimeout;
 	
 	Map<String, Object> producerConfigs() {
