e2df6456fe485642a18a95267cd6c696e855ab4d
Shen Tang <adatang23@yahoo.com>
2026-06-25 21:11:59 -0400

    Added idempotent configurations in the application.properties and KafkaConfig, tested using Postman Post request

--- section8/ProductsMicroservice/src/main/java/com/appsdeveloperblog/ws/products/KafkaConfig.java
+++ section8/ProductsMicroservice/src/main/java/com/appsdeveloperblog/ws/products/KafkaConfig.java
@@ -19,26 +19,32 @@ import com.appsdeveloperblog.ws.products.service.ProductCreatedEvent;
 @Configuration
 public class KafkaConfig {
 	
-	@Value("{spring.kafka.producer.bootstrap-servers}")
+	@Value("${spring.kafka.producer.bootstrap-servers}")
 	private String bootstrapServers;
 	
-	@Value("{spring.kafka.producer.key-serializer}")
+	@Value("${spring.kafka.producer.key-serializer}")
 	private String keySerializer;
 	
-	@Value("{spring.kafka.producer.value-serializer}")
+	@Value("${spring.kafka.producer.value-serializer}")
 	private String valueSerializer;
 	
-	@Value("{spring.kafka.producer.acks}")
+	@Value("${spring.kafka.producer.acks}")
 	private String acks;
 
-	@Value("{spring.kafka.producer.properties.delivery.timeout.ms}")
+	@Value("${spring.kafka.producer.properties.delivery.timeout.ms}")
 	private String deliveryTimeout;
 	
-	@Value("{spring.kafka.producer.properties.linger.ms}")
+	@Value("${spring.kafka.producer.properties.linger.ms}")
 	private String linger;
 	
-	@Value("{spring.kafka.producer.properties.request.timeout.ms}")
+	@Value("${spring.kafka.producer.properties.request.timeout.ms}")
 	private String requestTimeout;
+	
+	@Value("${spring.kafka.producer.properties.enable.idempotence}")
+	private boolean idempotence;
+	
+	@Value("${spring.kafka.producer.properties.max.in.flight.requests.per.connection}")
+	private Integer inflightRequests;
 	
 	Map<String, Object> producerConfigs() {
 		Map<String, Object> config = new HashMap<>();
@@ -50,6 +56,10 @@ public class KafkaConfig {
 		config.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeout);
 		config.put(ProducerConfig.LINGER_MS_CONFIG, linger);
 		config.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeout);
+		//default set is true, but explicitly set it will avoid disable it due to conflict configurations
+		config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, idempotence);
+		config.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, inflightRequests);
+		//config.put(ProducerConfig.RETRIES_CONFIG, Integer.MAX_VALUE);
 		
 		return config;
 	}
