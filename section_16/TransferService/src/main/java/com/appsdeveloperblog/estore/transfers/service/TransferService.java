90af08db73ed5203a959043f8ea3970ae469e639
Shen Tang <adatang23@yahoo.com>
2026-07-01 16:59:00 -0400

    Initial commit

---
+++ section_16/TransferService/src/main/java/com/appsdeveloperblog/estore/transfers/service/TransferService.java
@@ -0,0 +1,8 @@
+package com.appsdeveloperblog.estore.transfers.service;
+
+import com.appsdeveloperblog.estore.transfers.model.TransferRestModel;
+
+public interface TransferService {
+    public boolean transfer(TransferRestModel productPaymentRestModel);
+	//public <T> boolean transfer(TransferRestModel transferRestModel);
+}
