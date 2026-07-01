f1ccd464ecee0fd2fdb310c33392a6227579c285
Shen Tang <adatang23@yahoo.com>
2026-07-01 18:46:30 -0400

    Added configuration of transactionManager to handle both Kafka transactions and JPA transactions in transfers, and added the repository.save() method to save the transaction data into the H2 database, and tested with Postman

--- section_16/TransferService/src/main/java/com/appsdeveloperblog/estore/transfers/service/TransferServiceImpl.java
+++ section_16/TransferService/src/main/java/com/appsdeveloperblog/estore/transfers/service/TransferServiceImpl.java
@@ -2,8 +2,10 @@ package com.appsdeveloperblog.estore.transfers.service;
 
 import java.net.ConnectException;
 
+import org.apache.kafka.common.Uuid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -15,6 +17,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import com.appsdeveloperblog.estore.transfers.error.TransferServiceException;
+import com.appsdeveloperblog.estore.transfers.io.TransferEntity;
+import com.appsdeveloperblog.estore.transfers.io.TransferRepository;
 import com.appsdeveloperblog.estore.transfers.model.TransferRestModel;
 import com.appsdeveloperblog.payments.ws.core.events.DepositRequestedEvent;
 import com.appsdeveloperblog.payments.ws.core.events.WithdrawalRequestedEvent;
@@ -26,45 +30,41 @@ public class TransferServiceImpl implements TransferService {
 	private KafkaTemplate<String, Object> kafkaTemplate;
 	private Environment environment;
 	private RestTemplate restTemplate;
+	private TransferRepository transferRepository;
 
 	public TransferServiceImpl(KafkaTemplate<String, Object> kafkaTemplate, Environment environment,
-			RestTemplate restTemplate) {
+			RestTemplate restTemplate, TransferRepository transferRepository) {
 		this.kafkaTemplate = kafkaTemplate;
 		this.environment = environment;
 		this.restTemplate = restTemplate;
+		this.transferRepository = transferRepository;
 	}
 
-	@Transactional
+	//It is necessary to configure kafkaTransactionManager with @Bean in KafkaConfig
+	//This transactionManager will work with both Jpa and Kafka transaction
+	@Transactional("transactionManager")
 	@Override
 	public boolean transfer(TransferRestModel transferRestModel) {
 		WithdrawalRequestedEvent withdrawalEvent = new WithdrawalRequestedEvent(transferRestModel.getSenderId(),
 				transferRestModel.getRecepientId(), transferRestModel.getAmount());
 		DepositRequestedEvent depositEvent = new DepositRequestedEvent(transferRestModel.getSenderId(),
 				transferRestModel.getRecepientId(), transferRestModel.getAmount());
+		
+		TransferEntity transferEntity = new TransferEntity();
+		BeanUtils.copyProperties(transferRestModel, transferEntity);
+		transferEntity.setTransferId(Uuid.randomUuid().toString());
 
 		try {
-//			kafkaTemplate.send(environment.getProperty("withdraw-money-topic", "withdraw-money-topic"),
-//					withdrawalEvent);
-//			LOGGER.info("Sent event to withdrawal topic.");
-//		    callRemoteServce();
-//		    kafkaTemplate.send(environment.getProperty("deposit-money-topic", "deposit-money-topic"), 
-//		    		depositEvent);
-//		    LOGGER.info("Sent event to deposit topic");
-		    
-			boolean returnValue = kafkaTemplate.executeInTransaction(t -> {
-				t.send("withdraw-money-topic", withdrawalEvent);
-				LOGGER.info("Sent event to withdrawal topic.");
-				// Business logic that causes and error
-				try {
-					callRemoteServce();
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				t.send("deposit-money-topic", depositEvent);
-				LOGGER.info("Sent event to deposit topic");
-				return true;
-			});
+			// Save record to a database table
+			transferRepository.save(transferEntity);
+			
+			kafkaTemplate.send(environment.getProperty("withdraw-money-topic", "withdraw-money-topic"),
+					withdrawalEvent);
+			LOGGER.info("Sent event to withdrawal topic.");
+		    callRemoteServce();
+		    kafkaTemplate.send(environment.getProperty("deposit-money-topic", "deposit-money-topic"), 
+		    		depositEvent);
+		    LOGGER.info("Sent event to deposit topic");
 		} catch (Exception ex) {
 			LOGGER.error(ex.getMessage(), ex);
 			throw new TransferServiceException(ex);
