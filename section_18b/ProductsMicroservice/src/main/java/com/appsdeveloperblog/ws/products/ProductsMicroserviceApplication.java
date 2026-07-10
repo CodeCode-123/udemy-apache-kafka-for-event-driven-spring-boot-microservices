8f1db563338999cd3916cbeaff0d5ccee6d197d0
Shen Tang <adatang23@yahoo.com>
2026-07-10 00:33:58 -0400

    Reconfigured EmailNotificationMicroservice and added the ProductCreatedEventHandlerIntegrationTest

---
+++ section_18b/ProductsMicroservice/src/main/java/com/appsdeveloperblog/ws/products/ProductsMicroserviceApplication.java
@@ -0,0 +1,24 @@
+package com.appsdeveloperblog.ws.products;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import com.appsdeveloperblog.ws.products.service.ProductServiceImpl;
+import com.appsdevloperblog.ws.core.ProductCreatedEvent;
+
+@SpringBootApplication
+public class ProductsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsMicroserviceApplication.class, args);
+	}
+	
+	@Bean(name="productServiceImpl")
+	ProductServiceImpl getProductServiceImpl(KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate){
+		return new ProductServiceImpl(kafkaTemplate);
+	}
+
+}
